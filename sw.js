3ccbb7d70b5b5e83158782da962381721f798129
nathanielphilip-creator <nathaniel.philip@gmail.com>
2026-08-19 15:19:17 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,13 @@
+const CACHE = "synthesis-tracker-v1";
+const FILES = ["./", "./index.html", "./manifest.webmanifest"];
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES)));
+});
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
+});
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request))
+  );
+});
